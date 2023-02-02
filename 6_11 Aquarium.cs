b0bf9d81208c801a94b410154aeaecaed7a25200
Roman Chursin <105269016+romich9810@users.noreply.github.com>
2023-02-03 06:33:34 +0700

    Update 6_11 Aquarium.cs

--- 6_11 Aquarium.cs
+++ 6_11 Aquarium.cs
@@ -32,7 +32,7 @@ namespace _6_11_Aquarium
             {
                 if (aquarium.YearAfterBuy > 0)
                 {
-                    Console.WriteLine($"Прошло {aquarium.YearAfterBuy} после покупки аквариума.") ;
+                    Console.WriteLine($"Прошло {aquarium.YearAfterBuy} после покупки аквариума.");
                 }
 
                 aquarium.ShowFishes();
@@ -48,7 +48,7 @@ namespace _6_11_Aquarium
 
                 switch (userCommand)
                 {
-                    case WaitOneYearCommandNumber:                       
+                    case WaitOneYearCommandNumber:
                         aquarium.GrowOld();
                         break;
 
@@ -77,189 +77,190 @@ namespace _6_11_Aquarium
                 Console.Clear();
             }
         }
+    }
 
-        class Fish
+    class Fish
+    {
+        public Fish(string nameOfType, int age, int lifeExpentancy)
         {
-            public Fish (string nameOfType, int age, int lifeExpentancy)
+            NameOfType = nameOfType;
+            IsAlive = true;
+            Age = age;
+            LifeExpentancy = lifeExpentancy;
+        }
+
+        public string NameOfType { get; private set; }
+        public int Age { get; private set; }
+        public int LifeExpentancy { get; private set; }
+        public bool IsAlive { get; private set; }
+
+        public void GrowOld(Random random)
+        {
+            int minimalPercentOtherFactors = 5;
+
+            if (IsAlive)
             {
-                NameOfType = nameOfType;
-                IsAlive = true; 
-                Age = age;
-                LifeExpentancy = lifeExpentancy;
-            }
+                Age++;
 
-            public string NameOfType { get; private set; }
-            public int Age { get; private set; }
-            public int LifeExpentancy { get; private set; }
-            public bool IsAlive { get; private set; }
-
-            public void GrowOld(Random random)
-            {
-                int minimalPercentOtherFactors = 5;
-
-                if (IsAlive)
+                if (LifeExpentancy < Age)
                 {
-                    Age++;
+                    int percentOtherFactors = minimalPercentOtherFactors * (Age - LifeExpentancy);
 
-                    if (LifeExpentancy < Age)
-                    {
-                        int percentOtherFactors = minimalPercentOtherFactors * (Age - LifeExpentancy);
-
-                        InteractWithMicrobes(percentOtherFactors, random);
-                    }
-                    else
-                    {
-                        InteractWithMicrobes(0, random);
-                    }
+                    InteractWithMicrobes(percentOtherFactors, random);
                 }
-            }
-
-            private void InteractWithMicrobes(int percentOfInfluenceOtherFactors, Random random)
-            {
-                int minPercentOfProbabilty = 1;
-                int maxPercentOfProbabilty = 100;
-
-                int percentOfInfection = 15;
-
-                int percentOfProbabilty = random.Next(minPercentOfProbabilty, maxPercentOfProbabilty + 1);
-
-                if ((percentOfProbabilty  < percentOfInfection + percentOfInfluenceOtherFactors) & IsAlive)
+                else
                 {
-                    IsAlive = false;
-
-                    Console.WriteLine($"{NameOfType} заболела и умерла.");
+                    InteractWithMicrobes(0, random);
                 }
             }
         }
 
-        class Aquarium
+        private void InteractWithMicrobes(int percentOfInfluenceOtherFactors, Random random)
         {
-            private List<Fish> _allFishes;
+            int minPercentOfProbabilty = 1;
+            int maxPercentOfProbabilty = 100;
 
-            public Aquarium()
+            int percentOfInfection = 15;
+
+            int percentOfProbabilty = random.Next(minPercentOfProbabilty, maxPercentOfProbabilty + 1);
+
+            if ((percentOfProbabilty < percentOfInfection + percentOfInfluenceOtherFactors) & IsAlive)
             {
-                Random random = new Random();
+                IsAlive = false;
 
-                int maxCapacity = 10;
-                int minCapacity = 3;
-
-                Capacity = random.Next(minCapacity, maxCapacity + 1);
-
-                _allFishes = new List<Fish>();
-
-                _allFishes.Add(new Fish("Золотая рыбка", 5, 15));
-                _allFishes.Add(new Fish("Скалярия", 3, 10));
-
-                YearAfterBuy = 0;
+                Console.WriteLine($"{NameOfType} заболела и умерла.");
             }
+        }
+    }
 
-            public int YearAfterBuy { get; private set; }
+    class Aquarium
+    {
+        private List<Fish> _allFishes;
 
-            public int Capacity { get; private set; }
+        public Aquarium()
+        {
+            Random random = new Random();
 
-            public void AddFish()
+            int maxCapacity = 10;
+            int minCapacity = 3;
+
+            Capacity = random.Next(minCapacity, maxCapacity + 1);
+
+            _allFishes = new List<Fish>();
+
+            _allFishes.Add(new Fish("Золотая рыбка", 5, 15));
+            _allFishes.Add(new Fish("Скалярия", 3, 10));
+
+            YearAfterBuy = 0;
+        }
+
+        public int YearAfterBuy { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public void AddFish()
+        {
+            string nameOfType;
+
+            bool isAgeCorrectly;
+            bool isLifeExpentancyCorreclty;
+
+            if (Capacity <= _allFishes.Count)
             {
-                string nameOfType;
+                Console.WriteLine("Больше нельзя добавить рыбок - полный аквариум!");
+            }
+            else
+            {
+                Console.WriteLine($"Введите вид рыбки:");
+                nameOfType = Console.ReadLine();
 
-                bool isAgeCorrectly;
-                bool isLifeExpentancyCorreclty;
+                Console.WriteLine("Введите возраст рыбки:");
+                isAgeCorrectly = int.TryParse(Console.ReadLine(), out int age);
 
-                if (Capacity <= _allFishes.Count)
+                Console.WriteLine($"Введите среднюю продолжительность вида {nameOfType}:");
+                isLifeExpentancyCorreclty = int.TryParse(Console.ReadLine(), out int lifeExpentancy);
+
+                if ((isAgeCorrectly && isLifeExpentancyCorreclty) && (age > 0 && lifeExpentancy > 0))
                 {
-                    Console.WriteLine("Больше нельзя добавить рыбок - полный аквариум!");
+                    _allFishes.Add(new Fish(nameOfType, age, lifeExpentancy));
+                    Console.WriteLine($"{nameOfType} добавлена в аквариум.");
                 }
                 else
                 {
-                    Console.WriteLine($"Введите вид рыбки:");
-                    nameOfType = Console.ReadLine();
-
-                    Console.WriteLine("Введите возраст рыбки:");
-                    isAgeCorrectly = int.TryParse(Console.ReadLine(), out int age);
-
-                    Console.WriteLine($"Введите среднюю продолжительность вида {nameOfType}:");
-                    isLifeExpentancyCorreclty = int.TryParse(Console.ReadLine(), out int lifeExpentancy);
-
-                    if ((isAgeCorrectly && isLifeExpentancyCorreclty) && (age > 0 && lifeExpentancy > 0))
-                    {
-                        _allFishes.Add(new Fish(nameOfType, age, lifeExpentancy));
-                        Console.WriteLine($"{nameOfType} добавлена в аквариум.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неверный возраст или средняя продолжительность жизни.");
-                    }
+                    Console.WriteLine("Неверный возраст или средняя продолжительность жизни.");
                 }
             }
+        }
 
-            public void PickUpFish()
+        public void PickUpFish()
+        {
+            bool isIndexCorrect;
+
+            ShowFishes();
+
+            Console.WriteLine("\nВведите индекс рыбки:");
+            isIndexCorrect = int.TryParse(Console.ReadLine(), out int indexFish);
+
+            if (isIndexCorrect && indexFish > 0)
             {
-                bool isIndexCorrect;
+                _allFishes.RemoveAt(indexFish - 1);
 
-                ShowFishes();
+                Console.WriteLine("Рыбка убрана из аквариума.");
+            }
+            else
+            {
+                Console.WriteLine("Индекс рыбки некоректен!");
+            }
+        }
 
-                Console.WriteLine("\nВведите индекс рыбки:");
-                isIndexCorrect = int.TryParse(Console.ReadLine(), out int indexFish);
+        public void PickUpDeadFishes()
+        {
+            if (_allFishes.Exists(fish => fish.IsAlive == false) == false)
+            {
+                Console.WriteLine("Все рыбки живы ).");
+            }
+            else
+            {
+                _allFishes.RemoveAll(fish => fish.IsAlive == false);
 
-                if (isIndexCorrect && indexFish > 0)
+                Console.WriteLine("Вы похоронили мертвых рыбок.");
+            }
+        }
+
+        public void ShowFishes()
+        {
+            int index = 0;
+
+            Console.WriteLine($"Сейчас в аквариуме {_allFishes.Count} рыбок из {Capacity} возможных:");
+
+            foreach (Fish fish in _allFishes)
+            {
+                index++;
+
+                Console.Write($"{index}) {fish.NameOfType}, {fish.Age} лет, {fish.LifeExpentancy} лет средняя продолжительность жизни, ");
+
+                if (fish.IsAlive)
                 {
-                    _allFishes.RemoveAt(indexFish - 1);
-
-                    Console.WriteLine("Рыбка убрана из аквариума.");
+                    Console.WriteLine("жива.");
                 }
                 else
                 {
-                    Console.WriteLine("Индекс рыбки некоректен!");
+                    Console.WriteLine("умерла.");
                 }
             }
+        }
 
-            public void PickUpDeadFishes()
+        public void GrowOld()
+        {
+            Random random = new Random();
+
+            YearAfterBuy++;
+
+            foreach (Fish fish in _allFishes)
             {
-                if (_allFishes.Exists (fish => fish.IsAlive == false) == false)
-                {
-                    Console.WriteLine("Все рыбки живы ).");
-                }
-                else
-                {
-                    _allFishes.RemoveAll (fish => fish.IsAlive == false);
-
-                    Console.WriteLine("Вы похоронили мертвых рыбок.");
-                }
+                fish.GrowOld(random);
             }
-
-            public void ShowFishes()
-            {
-                int index = 0;
-
-                Console.WriteLine($"Сейчас в аквариуме {_allFishes.Count} рыбок из {Capacity} возможных:");
-
-                foreach(Fish fish in _allFishes)
-                {
-                    index++;
-
-                    Console.Write($"{index}) {fish.NameOfType}, {fish.Age} лет, {fish.LifeExpentancy} лет средняя продолжительность жизни, ");
-
-                    if (fish.IsAlive)
-                    {
-                        Console.WriteLine("жива.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("умерла."); 
-                    }
-                }
-            }
-
-            public void GrowOld()
-            {
-                Random random = new Random();
-
-                YearAfterBuy++;
-
-                foreach(Fish fish in _allFishes)
-                {
-                    fish.GrowOld(random);
-                }
-            } 
         }
     }
 }
+      
